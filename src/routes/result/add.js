164834605089ee3dbf04c2709567b3bfc03d24a3
Michiel <git@fastfox.nl>
2016-08-30 09:19:42 +0200

    In Add result, a select tag is used to select a rider

--- src/routes/result/add.js
+++ src/routes/result/add.js
@@ -1,24 +1,32 @@
 import {inject} from 'aurelia-framework';
 import {RaceService} from '../../services/data/race';
+import {RiderService} from '../../services/data/rider';
 import {Router, Redirect} from 'aurelia-router';
 
-@inject(RaceService, Router)
+@inject(RaceService, RiderService, Router)
 export class Add {
     rider;
     race;
+    races;
+    riders;
     result;
     note;
 
     submitted = false;
 
-    constructor(raceService, router) {
+    constructor(raceService, riderService, router) {
         this.raceService = raceService;
+        this.riderService = riderService;
         this.router = router;
     }
 
     activate(params) {
         this.raceService.load(params.slug, params.date).then(race => {
             this.race = race;
+        });
+
+        this.riderService.load().then(riders => {
+            this.riders = riders;
         });
     }
 
