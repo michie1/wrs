1b1d97d00fdd228581a19db92dedc3ac4a457c32
Michiel <git@fastfox.nl>
2016-09-06 22:15:24 +0200

    Also use newer in gulp tdd

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,10 @@ gulp.task('testbuild', () => {
     return gulp.watch('testtape/src/**/*.js')
        .on('change', (file) => {
           return gulp.src(file)
+              .pipe(newer(file.replace('src', 'build'))) // testing a file will result in a change, and newer prevents it.
               .pipe(babel())
               .pipe(gulp.dest((file) => {
+                  gutil.log('src -> build', file.base.replace('src', 'build'));
                   return file.base.replace('src', 'build');
               }))
        }); 
@@ -18,6 +20,7 @@ gulp.task('testbuild', () => {
 gulp.task('testwatch', () => {
     return gulp.watch('testtape/build/**/*.js')
                       .on('change', (file) => { 
+                          gutil.log('test', file);
                           return gulp.src(file).pipe(tape());
                       });
 });
