cbadfffb1f070d7cee424e9d183685ffa67a7874
Michiel <git@fastfox.nl>
2016-09-07 21:51:10 +0200

    create race test

--- testtape/src/services/data/race.js
+++ testtape/src/services/data/race.js
@@ -4,34 +4,80 @@ import {Race} from '../../../../build/models/race'
 
 let raceService = new RaceService(null);
 
-test('RaceService', function (t) {
-    t.ok(raceService, 'raceService should exist');
-    t.end();
-});
-
 let raceObject = {
     "name":"Ronde van de Lier",
     "date":"2016-08-11",
     "category":"criterium",
 };
 
+let expectedRace = new Race(raceObject);
+
+let raceObjects = [{
+    "name":"Gouden Pijl Emmen",
+    "date":"2016-08-11",
+    "category":"criterium",
+    "report":"verslag"
+}, {
+    "name":"Ronde van de Lier",
+    "date":"2016-08-11",
+    "category":"criterium",
+    "report":"verslag"
+}];
+
+let newRaceObject = {
+    name: 'Standaard Tijdelijk',
+    date: '2016-09-03',
+    category: 'criterium'
+};
+
+let newRaceResponse = {
+    message: 'success',
+    slug: 'standaard-tijdelijk',
+    date: '2016-09-03'
+};
+
+let expectedRaces = raceObjects.map((obj) => {
+    return new Race(obj);
+});
+
 raceService.fetchRace = function () {
+        return new Promise((resolve, reject) => {
+            resolve(raceObject);
+        });
+}
+
+raceService.fetchRaces = function () {
     return new Promise((resolve, reject) => {
-        resolve(raceObject);
+        resolve(raceObjects);
     });
 }
 
-test('RaceService 2', function (t) {
-    raceService.fetchRace().then((race) => {
-        t.same(race, raceObject, 'fetchRace correct');
-        t.end();
+raceService.create = function () {
+    return new Promise((resolve, reject) => {
+        resolve(newRaceResponse);
     });
-});
+};
+            
+test('RaceService', function (t) {
+    t.plan(5);
 
-test('RaceService 3', function (t) {
-    let expectedRace = new Race(raceObject);
-    raceService.load('ronde-van-de-lier', '2016-08-11').then(race => {
-        t.same(race, expectedRace, 'load correct');
-        t.end();
+    t.ok(raceService, 'raceService should exist');
+
+    raceService.fetchRace().then((race) => {
+        t.same(race, raceObject, 'fetchRace');
     });
+
+    raceService.load('ronde-van-de-lier', '2016-08-11').then(race => {
+        t.same(race, expectedRace, 'load one');
+    });
+
+    raceService.load().then(races => {
+        // Sort by date and name, because api does not return an ordered array
+        t.same(races.map(race => race.date + race.name).sort(), expectedRaces.map(race => race.date + race.name).sort(), 'load all');
+    });
+
+    raceService.create(newRaceObject)
+        .then((response) => {
+            t.same(response, newRaceResponse, 'create');
+        });
 });
