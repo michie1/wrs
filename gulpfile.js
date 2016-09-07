cd19e762c8d3d9efa8bacc512266601e71cb9888
Michiel <git@fastfox.nl>
2016-09-07 22:12:49 +0200

    Add plumber in order to continue tdd after syntax error

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ const newer = require('gulp-newer');
 const babel = require('gulp-babel');
 const gutil = require('gulp-util');
 const tape = require('gulp-tape');
+const faucet = require('faucet');
+const plumber = require('gulp-plumber');
 
 gulp.task('testbuild', () => {
     return gulp.watch('testtape/src/**/*.js')
        .on('change', (file) => {
           return gulp.src(file)
+              .pipe(plumber())
               .pipe(newer(file.replace('src', 'build'))) // testing a file will result in a change, and newer prevents it.
               .pipe(babel())
               .pipe(gulp.dest((file) => {
@@ -21,7 +24,12 @@ gulp.task('testwatch', () => {
     return gulp.watch('testtape/build/**/*.js')
                       .on('change', (file) => { 
                           gutil.log('test', file);
-                          return gulp.src(file).pipe(tape());
+                          return gulp.src(file)
+                              .pipe(plumber())
+                              .pipe(tape({
+                              reporter: faucet()
+                          
+                          }));
                       });
 });
 
