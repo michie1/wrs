156bd6957dce2bfd25eccc864106b505647932fb
Michiel <git@fastfox.nl>
2016-09-07 22:12:58 +0200

    Removed old commented code

--- src/services/data/race.js
+++ src/services/data/race.js
@@ -22,30 +22,14 @@ export class RaceService {
     }
 
     load(slug, date) {
-        if (slug === undefined) { // load all
-            /*
-            return this.http.fetch('races')
-                .then(response => {
-                    return response.json()
-                })
-                .then(races => {
-                    return races.map(data => {
-                        return new Race(data);
-                    });
-                });
-               */
+        if (slug === undefined) { 
             return this.fetchRaces()
                 .then(races => {
                     return races.map(data => {
                         return new Race(data);
                     });
                 });
-        } else { // load specific one
-            /*
-            return this.http.fetch('races/' + date +  '/' + slug)
-            .then(response => response.json())
-            .then(data => new Race(data));
-            */
+        } else { 
             return this.fetchRace(slug, date).then(data => new Race(data));
         }
     }
