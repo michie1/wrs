e6fc386b24289c2e08ea553873c10f6743ff8062
Michiel <git@fastfox.nl>
2016-09-11 15:53:46 +0200

    tdd also watches source files changes and not only tests

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const tape = require('gulp-tape');
 const faucet = require('faucet');
 const plumber = require('gulp-plumber');
 
-gulp.task('testbuild', () => {
+gulp.task('buildtest', () => {
     return gulp.watch('testtape/src/**/*.js')
        .on('change', (file) => {
           return gulp.src(file)
@@ -14,16 +14,44 @@ gulp.task('testbuild', () => {
               .pipe(newer(file.replace('src', 'build'))) // testing a file will result in a change, and newer prevents it.
               .pipe(babel())
               .pipe(gulp.dest((file) => {
-                  gutil.log('src -> build', file.base.replace('src', 'build'));
+                  gutil.log('watch test source and build test', file.base.replace('src', 'build'));
+                  return file.base.replace('src', 'build');
+              }))
+       }); 
+}):
+
+gulp.task('testwatchtest', () => {
+    return gulp.watch(['testtape/build/**/*.js'])
+        .on('change', (file) => { 
+            gutil.log('watch test build and test', file);
+            return gulp.src(file)
+                .pipe(plumber())
+                .pipe(tape({
+                    reporter: faucet()
+                }));
+        });
+});
+
+gulp.task('buildsource', () => {
+    return gulp.watch('src/**/*.js')
+       .on('change', (file) => {
+          return gulp.src(file)
+              .pipe(plumber())
+              .pipe(newer(file.replace('src', 'build'))) // testing a file will result in a change, and newer prevents it.
+              .pipe(babel())
+              .pipe(gulp.dest((file) => {
+                  gutil.log('watch source and build app', file.base.replace('src', 'build'));
                   return file.base.replace('src', 'build');
               }))
        }); 
 });
 
-gulp.task('testwatch', () => {
-    return gulp.watch(['testtape/build/**/*.js', 'build/**.*.js'])
+
+gulp.task('testwatchsource', () => {
+    return gulp.watch(['build/**/*.js'])
                       .on('change', (file) => { 
-                          gutil.log('test', file);
+                          file = 'testtape/' + file;
+                          gutil.log('watch build and test', file);
                           return gulp.src(file)
                               .pipe(plumber())
                               .pipe(tape({
@@ -32,4 +60,4 @@ gulp.task('testwatch', () => {
                       });
 });
 
-gulp.task('tdd', gulp.parallel('testbuild', 'testwatch'));
+gulp.task('tdd', gulp.parallel('buildtest', 'testwatchtest', 'buildsource', 'testwatchsource'));
